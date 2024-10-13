27437483744abef98fa685c91c7c69f4d41666de
Shivam Kumar <shivam.chandrayan@gmail.com>
2024-10-13 18:21:55 +0530

    fix test for adding two numbers with comma delimiter

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,4 +1,6 @@
 export function add(expression: string): number {
-  const result = parseInt(expression);
-  return isNaN(result) ? 0 : result;
+  const pieces: string[] = expression.split(",");
+  const num1: number = isNaN(parseInt(pieces[0])) ? 0 : parseInt(pieces[0]);
+  const num2: number = isNaN(parseInt(pieces[1])) ? 0 : parseInt(pieces[1]);
+  return num1 + num2;
 }
