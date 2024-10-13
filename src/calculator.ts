deaea0d2901fadb52298e56d1615ad1d548510ba
Shivam Kumar <shivam.chandrayan@gmail.com>
2024-10-13 18:26:28 +0530

    fix test for adding any amount of comma separated numbers

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,6 +1,9 @@
 export function add(expression: string): number {
   const pieces: string[] = expression.split(",");
-  const num1: number = isNaN(parseInt(pieces[0])) ? 0 : parseInt(pieces[0]);
-  const num2: number = isNaN(parseInt(pieces[1])) ? 0 : parseInt(pieces[1]);
-  return num1 + num2;
+  const result = pieces
+    .map((piece) => parseInt(piece))
+    .filter((num) => !isNaN(num))
+    .reduce((sum, num) => sum + num, 0);
+
+  return result;
 }
