49dc5bfd171b1200b0d933918da006f10137c6c4
Shivam Kumar <shivam.chandrayan@gmail.com>
2024-10-13 18:54:49 +0530

    fix test for handling custom delimiters

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,9 +1,22 @@
-export function add(expression: string): number {
-  const pieces: string[] = expression.split(/,|\n/g);
-  const result = pieces
+function splitNumbers(expression: string): string[] {
+  if (expression.startsWith("//")) {
+    const parts = expression.split("\n");
+    const delimiter = parts[0].slice(2);
+    const numbers = parts[1];
+    return numbers.split(delimiter);
+  }
+
+  return expression.split(/,|\n/g);
+}
+
+function sumNumbers(numbers: string[]): number {
+  return numbers
     .map((piece) => parseInt(piece))
     .filter((num) => !isNaN(num))
     .reduce((sum, num) => sum + num, 0);
+}
 
-  return result;
+export function add(expression: string): number {
+  const pieces = splitNumbers(expression);
+  return sumNumbers(pieces);
 }
